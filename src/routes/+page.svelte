6f52fbbf61f0ce7115a295f72f1ffc06513749d9
Ben Banister <desidiawvu@gmail.com>
2024-08-30 22:23:12 -0400

    Tossed in tailwind and skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import {Avatar} from '@skeletonlabs/skeleton'
+</script>
+
+<div class="flex justify-center items-center h-screen w-screen bg-slate-700">
+  <Avatar src="https://media.discordapp.net/attachments/1261377737755590752/1271912501189152819/shepard_test3.jpg?ex=66d36e53&is=66d21cd3&hm=24fa9e49e29ab13b48103cac4aac6d854d8361f6014a6aa7233751ade87b94d6&=&format=webp&width=905&height=905" width="w-32" rounded="rounded-full" />
+</div>
