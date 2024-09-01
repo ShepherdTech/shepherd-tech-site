8364509011058be516f94c0c2ea60ff1834063c7
louieb <7221365+belouie@users.noreply.github.com>
2024-09-01 17:47:35 -0400

    Remove discord url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class="flex justify-center items-center h-screen w-screen bg-slate-700">
-  <Avatar src="https://media.discordapp.net/attachments/1261377737755590752/1271912501189152819/shepard_test3.jpg?ex=66d36e53&is=66d21cd3&hm=24fa9e49e29ab13b48103cac4aac6d854d8361f6014a6aa7233751ade87b94d6&=&format=webp&width=905&height=905" width="w-32" rounded="rounded-full" />
+  <span class="text-slate-300">Hello World</span>
 </div>
