ccacc65e77704e0faa65a77f8898a7a4fab77e0b
Ben Banister <desidiawvu@gmail.com>
2024-11-12 16:24:18 -0500

    Add new pages for team, services, and home with basic layout

---
+++ src/routes/so-what/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { t } from '$lib/translations/index';
+
+    const pageName = 'Home';
+</script>
+
+<div class="flex flex-col justify-center items-center h-screen w-screen bg-customBrown">
+    <img src="favicon.png" alt="logo" class="w-20 h-20" />
+    <h2 class="text-white text-2xl">So what?</h2>
+</div>
