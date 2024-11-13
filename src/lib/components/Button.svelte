cfd8fdf582d4f59bd9159e7a4bc926781fbe74eb
louieb <7221365+belouie@users.noreply.github.com>
2024-11-12 23:00:01 -0500

    Add 'so what' page; add reusable styled button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	// Define allowed HTML button types
+	export let type: 'button' | 'submit' | 'reset' | null | undefined = 'button';
+	// Allow onClick handler
+	export let onClick = () => {};
+	// Add style prop
+	export let style: string = '';
+
+	// Combine default classes with any additional styles
+	const baseClasses =
+		'bg-black rounded-full border-[3px] border-goblin px-4 py-2 text-sm text-sandcastle transition-all duration-200 hover:border-green-600 hover:text-green-400';
+	$: combinedClasses = style ? `${baseClasses} ${style}` : baseClasses;
+</script>
+
+<button {type} on:click={onClick} class={combinedClasses}>
+	<slot></slot>
+</button>
